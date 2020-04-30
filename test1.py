5bff70f9554bd52c419e2ca4d3582de92e5b59a3
lokesh <loks.nk@gmail.com>
2020-04-30 00:37:49 -0700

    Dell_Laptop

---
+++ test1.py
@@ -0,0 +1,53 @@
+import openpyxl
+from pprint import pprint as pp
+
+wb = openpyxl.load_workbook("/home/lokesh/Documents/Cable.xlsx")
+print(wb.sheetnames)
+sheet = wb['Sheet1']
+
+# for row in sheet.values:
+#     for value in row:
+#         print(value)
+#     print()
+
+
+
+# row = list(sheet.rows)[2]
+#
+#
+# # for rowobjects in tuple(sheet.rows):
+# #     for cellval in rowobjects:
+# #         print(cellval.coordinate, cellval.value)
+# #     print()
+#
+# #
+# # c = range(0, sheet.max_column + 1)
+# # r = range(1, sheet.max_row + 1)
+# #
+# #
+# print(help(openpyxl))
+#
+# #
+# # #
+# # for c in range(1, sheet.max_column + 1):
+# #
+#
+#
+# # for rowobj in sheet.columns:
+# #     for cellobj in rowobj:
+# #         print(cellobj.coordinate, cellobj.value)
+# #     print()
+#
+# # for item in col[2]:
+# #     n_col = sheet.columns
+# #     print(item.value)
+#
+#
+# # t = tuple(sheet['A1':'A334'])
+# # for rowofcellobj in list(sheet['A1']]):
+# #     for cellobj in rowofcellobj:
+# #         print(cellobj.value)
+# #     # for cellobj in list(sheet.columns)[1]:
+# #     #     print(cellobj.value)
+# #
+#
