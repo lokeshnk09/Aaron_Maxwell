3f6dc420e2b792f967e072ef50861e5649cdb7f9
lokesh <loks.nk@gmail.com>
2020-05-01 02:42:20 -0700

    Dell_Laptop

--- test1.py
+++ test1.py
@@ -2,8 +2,31 @@ import openpyxl
 from pprint import pprint as pp
 
 wb = openpyxl.load_workbook("/home/lokesh/Documents/Cable.xlsx")
-print(wb.sheetnames)
+#print(wb.sheetnames)
 sheet = wb['Sheet1']
+
+for row in range(1, sheet.max_row + 1):
+    for col in range(1, sheet.max_column + 1):
+        while col < sheet.max_column:
+            print(sheet.cell(row, col).value, end=' ')
+            break
+        continue
+    print()
+
+
+
+
+
+
+
+
+
+
+
+        # state = sheet['A' + str(rowobj)].value
+
+
+
 
 # for row in sheet.values:
 #     for value in row:
