0567ca7f366b04fa525cfc8ec7df4e054c9b2ad4
lokeshnk09 <loks.nk@gmail.com>
2020-05-04 09:59:14 -0700

    Office_system

--- test1.py
+++ test1.py
@@ -1,15 +1,6 @@
-import openpyxl
-from pprint import pprint as pp
 
-wb = openpyxl.load_workbook("/home/lokesh/Documents/Cable.xlsx")
-#print(wb.sheetnames)
-sheet = wb['Sheet1']
+import time
 
-for row in range(1, sheet.max_row + 1):
-    for col in range(1, sheet.max_column + 1):
-        while col < sheet.max_column:
-            print(sheet.cell(row, col).value, end=' ')
-            break
-        continue
-    print()
-
+time.ctime()
+thismoment = time.ctime()
+time.ctime(thismoment)
